3f273752a46a4deec7de0493dd2f24ab1cce2aec
Alex Anderson <anderson.john.alexander@gmail.com>
2023-09-21 17:22:09 -0700

    Update utf8.h
    Drop shifte table using precalculated shifts

--- utf8.h
+++ utf8.h
@@ -32,7 +32,6 @@ utf8_decode(void *buf, uint32_t *c, int *e)
     static const int masks[]  = {0x00, 0x7f, 0x1f, 0x0f, 0x07};
     static const uint32_t mins[] = {4194304, 0, 128, 2048, 65536};
     static const int shiftc[] = {0, 18, 12, 6, 0};
-    static const int shifte[] = {0, 6, 4, 2, 0};
 
     unsigned char *s = buf;
     int len = lengths[s[0] >> 3];
@@ -53,14 +52,13 @@ utf8_decode(void *buf, uint32_t *c, int *e)
     *c >>= shiftc[len];
 
     /* Accumulate the various error conditions. */
-    *e  = (*c < mins[len]) << 6; // non-canonical encoding
-    *e |= ((*c >> 11) == 0x1b) << 7;  // surrogate half?
-    *e |= (*c > 0x10FFFF) << 8;  // out of range?
-    *e |= (s[1] & 0xc0) >> 2;
-    *e |= (s[2] & 0xc0) >> 4;
-    *e |= (s[3]       ) >> 6;
-    *e ^= 0x2a; // top two bits of each tail byte correct?
-    *e >>= shifte[len];
+    *e  = (*c < mins[len]); // non-canonical encoding
+    *e |= ((*c >> 11) == 0x1b) << 1;  // surrogate half?
+    *e |= (*c > 0x10FFFF) << 2;  // out of range?
+    *e |= (s[1] & 0xc0) << 1;
+    *e |= (s[2] & 0xc0) >> 1;
+    *e |= (s[3]       ) >> 3;
+    *e ^= 0x150; // top two bits of each tail byte correct?
 
     return next;
 }
